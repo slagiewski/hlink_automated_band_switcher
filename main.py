35caa19d5a4b8070dfa8b2ece8b547806bc215f9
s3blag <s3blag@github.com>
2020-03-15 13:05:58 +0100

    checks improvements

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speedtest
 import json
 import time
+from datetime import datetime
 
 from huawei_lte_api.Client import Client
 from huawei_lte_api.AuthorizedConnection import AuthorizedConnection
@@ -11,8 +12,8 @@ ROUTER_IP = "192.168.1.1"
 ROUTER_PASSWORD = ""
 ### Router Access Credentials ###
 
-VALIDATION_SECOND_CHECK_DELAY_SECS = 120
-CONFIG_CHANGES_SAVE_WAIT_SECS = 30
+VALIDATION_SECOND_CHECK_DELAY_SECS = 15
+CONFIG_CHANGES_SAVE_WAIT_SECS = 5
 
 ONLY_4G_MODE = "03"
 ONLY_3G_MODE = "02"
@@ -30,14 +31,14 @@ NETWORK_CONFIGS = {
     "3G": {"network_mode": ONLY_3G_MODE, "network_band": ALL_SUPPORTED_3G_BANDS_CODE, "lte_band": EU_LTE_BANDS_CODE}
 }
 
-MIN_ACCEPTABLE_SPEED_MBITS = 2.0
+MIN_ACCEPTABLE_SPEED_MBITS = 1.5
 MIN_ACCEPTED_PING_MS = 350
 
 
 def __test_download_speed():
     tests_performed_count = 0
     while tests_performed_count < 2:
-        time.sleep(15)
+        time.sleep(10)
         try:
             speedtester = speedtest.Speedtest()
             speedtester.get_best_server()
@@ -53,7 +54,7 @@ def test_download_speed(test_id):
     print(f"Testing download speed ({test_id})...")
     (result_Mbits, ping) = __test_download_speed()
     print(
-        f"Received download speed results ({test_id}): \n{result_Mbits}Mb/s \n{ping}ms\n")
+        f"Received download speed results ({test_id}): \n{result_Mbits}Mb/s \n{ping}ms")
     return (result_Mbits, ping)
 
 
@@ -65,14 +66,16 @@ def __download_speed_valid(download_speed_Mbit, ping):
 
 
 def download_speed_valid():
-    if __download_speed_valid(*test_download_speed("1/2")):
+    (download_Mbits, ping) = test_download_speed("1/2")
+    if __download_speed_valid(download_Mbits, ping):
         print(f"Waiting {VALIDATION_SECOND_CHECK_DELAY_SECS}s...")
         time.sleep(VALIDATION_SECOND_CHECK_DELAY_SECS)
-        if __download_speed_valid(*test_download_speed("2/2")):
+        (download_Mbits, ping) = test_download_speed("2/2")
+        if __download_speed_valid(download_Mbits, ping):
             print("Results are OK!")
-            return True
+            return (True, download_Mbits, ping)
     print("Results are bad!")
-    return False
+    return (False, download_Mbits, ping)
 
 
 def change_router_config(config_name, network_band_config):
@@ -109,9 +112,17 @@ def change_router_config_to_best_config_of_invalid(best_config_of_invalid):
     change_router_config(
         best_config_of_invalid['name'], NETWORK_CONFIGS[best_config_of_invalid['name']])
 
+def print_log_entry_separator():
+    print("\n\n\n")
+
+def current_datetime():
+    return f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}]\n"
 
 def main():
-    if download_speed_valid():
+    print_log_entry_separator()
+    print(current_datetime())
+    (test_succeeded, result_Mbits, ping) = download_speed_valid()
+    if test_succeeded:
         return
 
     best_config_of_invalid = {
@@ -120,15 +131,13 @@ def main():
     }
     for config_name, config in NETWORK_CONFIGS.items():
         change_router_config(config_name, config)
-        if download_speed_valid():
+        (test_succeeded, result_Mbits, ping) = download_speed_valid()
+        if test_succeeded:
             return
-
-        (result_Mbits, _) = test_download_speed("1/1")
         update_best_config_of_invalid(
             best_config_of_invalid, config_name, result_Mbits)
     else:
         change_router_config_to_best_config_of_invalid(best_config_of_invalid)
-
 
 if __name__ == "__main__":
     main()
